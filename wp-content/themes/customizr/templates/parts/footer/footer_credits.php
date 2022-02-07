11a869741d53801a958905c2ed5de4da2a24c5ca
alexinfopruna <alexinfopruna@gmail.com>
2022-02-07 09:06:51 +0100

    preparant server

--- wp-content/themes/customizr/templates/parts/footer/footer_credits.php
+++ wp-content/themes/customizr/templates/parts/footer/footer_credits.php
@@ -9,11 +9,11 @@
     <span class="czr-copyright-text">&copy;&nbsp;<?php echo esc_html( date('Y') ) ?>&nbsp;</span><a class="czr-copyright-link" href="<?php echo esc_url( home_url() ) ?>" title="<?php echo esc_attr( get_bloginfo() ) ?>"><?php echo esc_html( get_bloginfo() ) ?></a><span class="czr-rights-text">&nbsp;&ndash;&nbsp;
         
             <?php _e( 'Carrer del Bonsuccés, 13, 08001, Barcelona', 'customizr') ?>
-        &#9742;
+        -
         
 
             <?php _e( '+34 933 99 34 52', 'customizr') ?>
-        &#x2709;
+        -
             <?php _e( 'qubbaarquitectes@gmail.com', 'customizr') ?>
     
     </span>
