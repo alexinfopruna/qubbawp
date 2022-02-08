912862fabebbb9e1d80f07878d91291989ceddfc
alexinfopruna <alexinfopruna@gmail.com>
2022-02-08 10:05:38 +0100

    search hidden

--- wp-content/plugins/lightgallerywp/features/features.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(dirname(__FILE__).'/invoke-scripts.php');
-require_once(dirname(__FILE__).'/gutenberg/gutenberg.php');
-require_once(dirname(__FILE__).'/custom-gallery/lightgallery-cpt.php');
-?>
--- wp-content/plugins/lightgallerywp/smartlogix-wrappers/wrappers.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once(dirname(__FILE__).'/smartlogix-controls-wrapper.php');
-require_once(dirname(__FILE__).'/smartlogix-settings-wrapper.php');
-require_once(dirname(__FILE__).'/smartlogix-cpt-wrapper.php');
-?>
