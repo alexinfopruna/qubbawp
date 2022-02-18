7174ce7d4d4c7aad5dfba45ed417ad4bf3ccfca7
alexinfopruna <alexinfopruna@gmail.com>
2022-02-18 16:35:24 +0100

    galeria 1column

--- wp-content/themes/customizr-qubba/qubba.js
+++ wp-content/themes/customizr-qubba/qubba.js
@@ -7,7 +7,6 @@
 
 jQuery(window).load(function() {
     jQuery("img").each(function() {
-
         // Calculate aspect ratio and store it in HTML data- attribute
         var aspectRatio = jQuery(this).width()/jQuery(this).height();
         jQuery(this).data("aspect-ratio", aspectRatio);
@@ -17,18 +16,21 @@ jQuery(window).load(function() {
             // Image is landscape
 
             jQuery( this ).addClass( "landscape" );
-
+console.log("landscape");
         } else if (aspectRatio < 1) {
             // Image is portrait
-            jQuery( this ).addClass( "portrait" );;
+            jQuery( this ).addClass( "portrait" );
+            console.log("portrait");
         } else {
             // Image is square
-            jQuery( this ).addClass( "square" );;            
+            jQuery( this ).addClass( "square" );          
+            console.log("square");
         }
+        /*
         setTimeout(() => {
   window.dispatchEvent(new Event('resize'));
 }, 300);
 setTimeout(() => {
   window.dispatchEvent(new Event('resize'));
-}, 300);
+}, 300);*/
     })});
