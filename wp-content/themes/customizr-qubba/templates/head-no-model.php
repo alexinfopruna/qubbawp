46a1da23670734b0238313004a1de7ca0b06871e
alexinfopruna <alexinfopruna@gmail.com>
2022-02-25 13:44:32 +0100

    verify google

--- wp-content/themes/customizr-qubba/templates/head-no-model.php
+++ wp-content/themes/customizr-qubba/templates/head-no-model.php
@@ -9,6 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-Frame-Options" content="sameorigin">
   <meta http-equiv="X-Content-Type-Options" content="nosniff">
+  <meta name="google-site-verification" content="uKEdCGMJ8fsqPnPC30jmHrzWF5JExvfXVBS0YQrqA6M" />
   <link rel="profile"  href="https://gmpg.org/xfn/11" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
